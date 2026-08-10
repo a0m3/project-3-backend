470438344eb743bd29663585ff9bd5330f79db97
mohasa7an22 <mohasar7an22@gmail.com>
2026-08-10 09:56:28 +0300

    made routes for custom games

---
+++ routes/customGame.routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const verifyToken = require('../middleware/verifyToken')
+const validateQuestions = require('../middleware/validateQuestion')
+const customGamecontroller = require('../controllers/customGames.controller')
+
+router.post('/', verifyToken, validateQuestions, customGamecontroller.createCustomGame)
+
+router.get('/', verifyToken, customGamecontroller.getMyCustomGames)
+
+router.get('/:id', verifyToken, customGamecontroller.getGameById)
+
+router.put('/:id', verifyToken, validateQuestions, customGamecontroller.updateCustomGame)
+
+router.delete('/:id', verifyToken, customGamecontroller.deleteGame)
+
+router.get('/:id/play', verifyToken, customGamecontroller,playGame)
+
+module.exports = router
